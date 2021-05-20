653d0eadedb662103a8c6d4385951555bad82e38
lk22 <leoknudsen@Leos-MBP.localdomain>
2021-05-20 14:57:57 +0200

    working on learning command for subject categories

--- commands/learn.js
+++ commands/learn.js
@@ -1,18 +1,48 @@
 const axios = require('axios')
 const fs = require('fs');
+const { url } = require('inspector');
 module.exports = {
     name: 'learn',
     description: 'Make DocsBot learning a new path to a current documentation site with a new url',
     execute(message, args, urls) {
-        console.log(urls);
-        
         let documentation = urls.filter((url) => url.name === args[0]);
         if (args.length < 1) {
             message.channel.send("I need to learn a name for the documentation");
         } else {
             if (documentation.length) {
                 if (args[0] === documentation[0].name) {
-                    message.channel.send(`I know the documentation ${args[0]} run /docs ${args[0]}`);
+
+                    const documentationCategories = documentation.categories;
+
+                    /**
+                     * assume second argument is -category= and third argument is -url to the category
+                     */
+                    if (args[1].indexOf("-category=") > -1 && args[2].indexOf("-url=") > -1) {
+                        
+                        const newCategory = (documentationCategories) ? [...documentationCategories, {
+                            name: args[1].replace("-category=", ""),
+                            url: args[2].replace("-url=", "")
+                        }] : [
+                                {
+                                    name: args[1].replace("-category=", ""),
+                                    url: args[2].replace("-url=", "")
+                            }
+                        ]
+
+                        Object.assign(documentation[0], { categories: newCategory })
+
+                        console.log(documentation);
+                        urls.push(documentation[0]);
+                        fs.open("documentations.json", 'w', (err) => {
+                            if (err) { console.log(err); return; }
+                            fs.writeFileSync('documentations.json', JSON.stringify(documentation, null, 2));
+                        });
+
+                        return;
+                    } else {
+                        message.channel.send(`I know the documentation ${args[0]} run /docs ${args[0]}`);
+                    }
+
                 } else {
                     documentation[0].name = args[0];
                     message.channel.send(`Documentation ${args[0]} name updated -> ${args[0]}`);
@@ -37,10 +67,12 @@ module.exports = {
                 } else {
                     newDocumentation.url = args[1];
                     urls.push(newDocumentation);
+
                     fs.open("documentations.json", 'w', (err) => {
                         if (err) { console.log(err); return; }
                         fs.writeFileSync('documentations.json', JSON.stringify(urls, null, 2));
                     })
+
                     message.channel.send(`I have now learned the documentation to "${newDocumentation.name}" with following url: "${newDocumentation.url}" you can now run /find git to visit git documentation.`);
                 }
             }
@@ -48,16 +80,3 @@ module.exports = {
     }
 }
 
-/**
- * update file helper
- * @param {*} file 
- * @param {*} fileMode 
- */
-function updateDocumentationList(file, fileMode, data) {
-    return fs.open(file, fileMode, (err) => {
-        if (err) {
-            console.log(err);
-            fs.writeFileSync(file, data)
-        }
-    })
-}
